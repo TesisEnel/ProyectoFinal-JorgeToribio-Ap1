b903661a8b2248cd7ab4b7beb355963e66babb82
JorgeLT1 <jorgeluistoribio14@gmail.com>
2023-04-02 22:48:11 -0400

    Update

---
+++ Models/Venta.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Venta
+{
+    [Key]
+    public int VentaId { get; set; }
+    public string? Concepto { get; set; }
+    public DateTime Fecha { get; set; }
+    public int clienteId { get; set; }
+    public int CarroId { get; set; }
+    public double PrecioVenta { get; set; }
+    
+    [ForeignKey("VentaId")]
+    public List<VentaDetalle> VentaDetalle { get; set; }
+
+}
