c2ddc49a73452cfd304c57f862bb862b8db0eaa4
JorgeLT1 <jorgeluistoribio14@gmail.com>
2023-04-08 17:16:32 -0400

    Eliminar Update

--- BLL/VentaBLL.cs
+++ BLL/VentaBLL.cs
@@ -12,13 +12,11 @@ public class VentaBLL{
     }
     
     private bool Insertar(Venta venta) {
-       InsertarDetalle(venta);
         _Contexto.Venta.Add(venta);
         return _Contexto.SaveChanges() > 0;
     }
 
   private bool Modificar(Venta venta) {
-    ModificarDetalle(venta);
     var VentaExistencia = _Contexto.Venta.Find(venta.VentaId);
     if (VentaExistencia == null) {
         return false;
@@ -39,7 +37,29 @@ public class VentaBLL{
 
 public bool Eliminar(int ventaId)
 {
-    return false;
+    var venta = _Contexto.Venta.Include(p => p.VentaDetalle).FirstOrDefault(p => p.VentaId == ventaId);
+
+    if (venta != null)
+    {
+        foreach (var detalle in venta.VentaDetalle)
+        {
+            var carro = _Contexto.Carro.Find(detalle.VentaId);
+            if (carro == null)
+            {
+                continue;
+            }
+        }
+
+        _Contexto.RemoveRange(venta.VentaDetalle);
+        _Contexto.Entry(venta).State = EntityState.Deleted;
+
+        int filasAfectadas = _Contexto.SaveChanges();
+        return filasAfectadas > 0;
+    }
+    else
+    {
+        return false;
+    }
 }
 
     public Venta? Buscar(int ventaId){
